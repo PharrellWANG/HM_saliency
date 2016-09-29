86ca7b64a528728c4c8dd90269ec630a9d16787d
anserw <x-zy@live.cn>
2016-09-29 19:47:43 +0800

    Fix a bug when built with opencv 2.*

--- Lib/TLibEncoder/TEncPreanalyzer.cpp
+++ Lib/TLibEncoder/TEncPreanalyzer.cpp
@@ -37,9 +37,9 @@
 
 #include <cfloat>
 #include <algorithm>
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <vector>
 
 #include "TEncPreanalyzer.h"
